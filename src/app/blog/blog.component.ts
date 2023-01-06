ec86f771a1deb9ed3cf552d551d540e2760b86c1
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-01-06 15:00:34 -0600

    add tailwind markdown styles

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss'],
-  imports: [CommonModule, ScullyLibModule],
+  imports: [CommonModule, ScullyLibModule, RouterModule],
   standalone: true,
 })
 export class BlogComponent implements OnInit {
