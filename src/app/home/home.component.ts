8252fbf8d29f6b66d98544b29038a2da2208c7c3
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-01-06 12:41:55 -0600

    update styles, add blog list page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../shared/components/header/header.component';
+import { SocialsComponent } from '../shared/components/social/social.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, SocialsComponent, RouterModule],
 })
 export class HomeComponent implements OnInit {
   open = false;
