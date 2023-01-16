b3b7c23d8ae85b2d09c53ddd83ae52d9e3de5a58
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-01-16 15:29:27 -0600

    add 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { BlogListComponent } from './pages/blog-list/blog-list.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { TipsComponent } from './pages/tips/tips.component';
 
 import { TweetWidgetComponent } from './shared/components/tweet-widget/tweet-widget.component';
@@ -32,6 +33,14 @@ const routes: Routes = [
   {
     path: 'tips/:tweetId',
     component: TweetWidgetComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+  {
+    path: '404',
+    component: PageNotFoundComponent,
   },
 ];
 
---
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './page-not-found.component.html',
+  styles: [],
+})
+export class PageNotFoundComponent {}
