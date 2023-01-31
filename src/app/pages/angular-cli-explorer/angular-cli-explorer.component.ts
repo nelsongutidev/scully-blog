f200b109e6c532c89ef25cdbe64dd38055537016
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-01-30 22:30:23 -0600

    add material and cli explorer component initial logic

--- src/app/pages/angular-cli-explorer/angular-cli-explorer.component.ts
+++ src/app/pages/angular-cli-explorer/angular-cli-explorer.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TerminalComponent } from 'src/app/shared/components/terminal/terminal.component';
+import { MatSelectModule } from '@angular/material/select';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { PRIMARY_OPTIONS, SECONDARY_OPTIONS } from './data/data';
+import { combineLatest, map, Observable, ReplaySubject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-angular-cli-explorer',
   standalone: true,
-  imports: [CommonModule, TerminalComponent],
+  imports: [
+    CommonModule,
+    TerminalComponent,
+    MatSelectModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './angular-cli-explorer.component.html',
   styles: [],
 })
-export class AngularCliExplorerComponent {}
+export class AngularCliExplorerComponent {
+  primaryOption = new FormControl();
+  secondaryOption = new FormControl();
+
+  primaryOptions = PRIMARY_OPTIONS;
+  secondaryOptions = SECONDARY_OPTIONS;
+
+  command$: Observable<string> = this.secondaryOption.valueChanges.pipe(
+    map((value) => value.command)
+  );
+
+  ngOnInit() {
+    console.log('init');
+  }
+}
