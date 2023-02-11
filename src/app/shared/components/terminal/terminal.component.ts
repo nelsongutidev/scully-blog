142bffc8ac17328022a8a2977e244f49079f2b05
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-02-10 23:05:56 -0600

    Implement toast and copy to clipboard

--- src/app/shared/components/terminal/terminal.component.ts
+++ src/app/shared/components/terminal/terminal.component.ts
@@ -1,13 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-terminal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatSnackBarModule],
   templateUrl: './terminal.component.html',
   styles: [],
 })
 export class TerminalComponent {
-  @Input() command = 'ng new my-app';
+  constructor(private clipboard: Clipboard, private snackBar: MatSnackBar) {}
+  @ViewChild('snackBarTpl') snackBarTpl: TemplateRef<any> | undefined;
+
+  @Input() command = '';
+
+  copyToClipboard() {
+    this.clipboard.copy(this.command);
+    this.openSnackBar();
+  }
+
+  openSnackBar() {
+    this.snackBar.openFromTemplate(this.snackBarTpl!, {
+      horizontalPosition: 'right',
+      duration: 3000,
+    });
+  }
 }
