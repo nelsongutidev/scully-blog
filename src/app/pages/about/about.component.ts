d077382f6ff519ba0be02351f615809dc4c98890
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-08-02 22:07:06 -0600

    add ngOptimizedImage

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   standalone: true,
+  imports: [NgOptimizedImage],
 })
 export class AboutComponent implements OnInit {
   yearsOfExperience = new Date().getFullYear() - 2017;
